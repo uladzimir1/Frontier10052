f11f1b929a356617915f44abb668b594ff1f474a
Uladzimir <uladzimir@gmail.com>
2026-07-15 17:13:33 +0200

    feat: configure Blazor application host

---
+++ src/Frontier10052.Web/Program.cs
@@ -0,0 +1,23 @@
+using Frontier10052.Web.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+    .AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseAntiforgery();
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
